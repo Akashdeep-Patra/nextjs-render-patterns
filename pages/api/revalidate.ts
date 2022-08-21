bf4e757e9d0bbc9d309f6440211d2e333c03e425
Akashdeep <adeep8961@gmail.com>
2022-08-21 16:04:02 +0530

    feat: isr revalidation TOKEN NAME

--- pages/api/revalidate.ts
+++ pages/api/revalidate.ts
@@ -11,7 +11,7 @@ export default async function handler(
   res: NextApiResponse<Data>
 ) {
   // Check for secret to confirm this is a valid request
-  if (req.query.secret !== process.env.MY_SECRET_TOKEN) {
+  if (req.query.secret !== process.env.REVALIDATION_TOKEN) {
     return res.status(401).json({ message: 'Invalid token' });
   }
   const pathToRevalidate = req.query?.pathToRevalidate as string;
